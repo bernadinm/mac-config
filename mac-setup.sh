832ff768ad5b95c712331b04a5b10fe1b561332b
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2022-07-20 13:50:53 -0700

    mac: installing maven

--- mac-setup.sh
+++ mac-setup.sh
@@ -55,6 +55,7 @@ brew install pwgen
 brew install go-jira
 brew install graphviz
 brew install python3 && sudo easy_install pip
+brew install maven
 brew install bash; sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
 brew tap saulpw/vd # visidata
 brew install visidata
